c75974f1b568ea3e609b4ddb4b8c67769a5b9a1d
Radu Topala <radu.topala@trisoft.ro>
2025-11-12 12:59:53 +0200

    Remove temporary test scripts

--- test_servers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Quick test - manually start mock servers and try to connect
-go test -v -tags=integration ./integration/... -run "TestHTTPIntegrationSuite/TestStreamableHTTPListTools" 2>&1 | tail -30
-
